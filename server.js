2c4e7941ccccae57f97d35d52335a88b9b83bd58
BuildTools <unconfigured@null.spigotmc.org>
2023-11-11 20:29:31 -0700

    added correct port

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ app.get('/', (req, res) => {
 })
 
 
-const PORT = 3000;
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
 
